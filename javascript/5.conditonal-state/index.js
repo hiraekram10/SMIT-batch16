31fd61da5d0f3677641700694f1966551b69faa0
tayyaba-ali <ta6402792@gmail.com>
2025-03-04 10:33:40 +0500

    started with conditonal statement

---
+++ javascript/5.conditonal-state/index.js
@@ -0,0 +1,29 @@
+
+
+// if(10 > 9){
+//     console.log("true sahi bat hai");
+    
+//   }else{
+//     console.log("false ghlt bat hai");
+//   }
+
+
+
+
+if(9 > 9){
+        console.log("true sahi bat hai");
+        
+      }else{
+        console.log("false ghlt bat hai");
+      }
+
+    let userInp = Number(prompt("enter one number"))
+
+
+    if(userInp < 10){
+        console.log("you are eligible");
+        
+    }else{
+        console.log("next time ana");
+        
+    }
