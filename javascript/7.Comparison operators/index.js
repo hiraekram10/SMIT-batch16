a988d6566b5d0f9cddaf911166433fdfa4d33794
Umra-Badar20 <umrabadar555@gmail.com>
2025-03-07 10:31:53 +0500

    comparasion operTOR HALF DONE

---
+++ javascript/7.Comparison operators/index.js
@@ -0,0 +1,85 @@
+// var num = +prompt('add one number')
+
+
+// if(num == 10){
+//     console.log("your are qualified");
+    
+// }else{
+//     console.log("not qualified");
+    
+// }
+
+
+// var num = prompt('add one number')
+// console.log(typeof num);// type checking
+
+
+// if(num === 12){
+//         console.log("your are qualified");
+        
+//     }else{
+//         console.log("not qualified");
+        
+//     }
+
+
+
+// console.log("22" == 10); //false
+// console.log(5 ==5); //true
+// console.log(5 == "5"); //true
+// console.log(5 === "5")  //false
+// console.log(245 === "245"); //false
+// console.log(false == "0");
+
+// let  num1 =  "false"
+// console.log(++num1); //Nan //num1 = num1 + 1
+
+// console.log(num1);
+
+
+// let  num =  "false"
+// console.log("false" + 1); //false1
+// console.log(num);
+
+
+
+
+// let  num1 =  "false"
+// console.log(++num1);   //num1 = num1 + 1
+
+
+// let storedPassIndb ="hira@12";
+// let userInp = prompt("Ener your password");
+
+
+// if(storedPassIndb === userInp){
+//     console.log("you are succesfully login");
+    
+// }else{
+//     console.log("your pass is incorrect");
+    
+// }
+
+
+
+console.log(10 > 9); //true
+console.log(3 < 9); //''
+console.log(10 >= 10); //''
+console.log(10 <= 4); //false
+console.log(3 <= 4);  // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
