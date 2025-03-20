fba7c2e8789f7b93c637fa20f5dd2a3a71888b31
tayyaba-ali <ta6402792@gmail.com>
2025-03-20 10:31:53 +0500

    done with array

---
+++ javascript/10.array/index.js
@@ -0,0 +1,82 @@
+// let student1 = 'rabia'
+// let student2 = 'rabia'
+// let student3 = 'rabia'
+
+
+// let students = ['ali' ,'aslam','haya','rabia']
+// console.log(students.length);  //4
+
+// index --> postition 0
+// console.log(students);
+
+
+
+// let students = ['ali' ,'aslam','haya','rabia']
+// console.log(students[2]);
+// console.log(students[0]);
+// console.log(students[3]);
+// console.log(students[1]);
+
+
+
+// let valuesofArr = ["hira" , 1, true, undefined]
+
+
+// let fruits =[];
+// console.log(fruits);
+
+// fruits[0] = 'anam'
+// fruits[2] = 'rida'
+
+
+// console.log(fruits);  // ['anam', empty, 'rida']
+
+
+
+// let fruits = ['apple','bnana']
+// console.log(fruits[2]);  //undefined
+
+
+// ____________________________________________________primitive (stack)
+// ______________________________________________________copy by value
+
+// var num = 1 
+// var num2 = num
+// console.log(num);
+// console.log(num2);
+
+
+
+// num2 = 2
+// console.log(num);
+// console.log(num2);
+
+
+// ______________________________________________non-primitive (heap)
+// _________________________________copy by refrence
+var names = ["hirA","rida"]
+var copyarr = names
+
+console.log(names);
+console.log(copyarr);
+
+
+
+copyarr[1] = 'haya'
+console.log(names);
+console.log(copyarr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javascript/9.logicalOperator/inde.js
+++ javascript/9.logicalOperator/inde.js
@@ -1,5 +1,4 @@
-// 
-
+//
 
 // console.log(9 <10 && 10==10 && 5==5);
 // console.log(true && 0 && 1 );  //0
@@ -10,9 +9,6 @@ console.log(0 && false && true);  //0
 console.log(true && 1 && "true" && -0 && 'four');   //-0
 console.log(true && 1 && "true"  && 'four'); //four
 */
-
-
-
 
 // logical or||
 // _____________________________________1.retrns first trthy value
@@ -26,39 +22,44 @@ console.log(true && 1 && "true"  && 'four'); //four
 // {products && products.map}
 // {phone || email} //optional input
 
-
 //_______________________________________________program
 
-let distance = '2k'
-let time = 15 
-let height = 5.7
-let weight = 80 
-let age = 25
+// let distance = '2k'
+// let time = 15
+// let height = 5.7
+// let weight = 80
+// let age = 25
 
-if(time < 20 && distance == '3k' || age < 40){
-  console.log('you are eligible');
-  
-}else{
-  console.log('ghr jao');
-  
+// if(time < 20 && distance == '3k' || age < 40){
+//   console.log('you are eligible');
+
+// }else{
+//   console.log('ghr jao');
+
+// }
+
+var percentage = +prompt("enter your percentage");
+
+if (percentage <= 100  && percentage >= 40) {  
+
+
+  if (percentage >= 90) {
+    console.log("A+");
+     } else if (percentage >= 80) {
+     console.log("A");
+      }
+      else if (percentage >= 70) {
+       console.log("b");
+        }
+        else if (percentage >= 60) {
+         console.log("c");
+          } else  {
+           console.log("fail");
+            }
+
+       
+
+
+} else {
+  console.log("this number is not valid");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
